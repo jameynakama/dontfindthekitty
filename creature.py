d70b9d41020aba635086d94acf916a52f3cf02b5
Jamey J. DeOrio <jameyd@darkhorse.com>
2013-09-13 20:48:24 -0700

    Fix bug with creature movement

--- creature.py
+++ creature.py
@@ -5,24 +5,31 @@ from constants import *
 
 
 class Creature:
-    def __init__(self, window):
-        self.character = unichr(random.randint(33, 127))
-        self.color = pygame.Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    def __init__(self):
+        self.character = str(random.randint(33, 127))
+        self.color = pygame.Color(random.randint(100, 255), random.randint(100, 255), random.randint(100, 255))
         self.sex = random.choice(('male', 'female'))
         self.xpos = random.randint(0, ZOO_WIDTH)
         self.ypos = random.randint(0, ZOO_HEIGHT)
 
+    def move(self):
+        newx = 0
+        newy = 0
+
+        while 1:
+            direction = random.choice(((0, -1), (0, 1), (-1, 0), (1, 0)))
+            newx = self.xpos + direction[0]
+            newy = self.ypos + direction[1]
+            if (0 <= newx < ZOO_WIDTH) and (0 <= newy < ZOO_HEIGHT):
+                break
+
+        self.xpos = newx
+        self.ypos = newy
+
     def draw(self, window):
-        window.write(
+        window.putchar(
             self.character,
             x=self.xpos,
             y=self.ypos,
             fgcolor=self.color,
         )
-
-    def move(self):
-        newx = self.xpos - 1
-        newy = self.ypos - 1
-        if (0 < newx < ZOO_WIDTH) and (0 < newy < ZOO_HEIGHT):
-            self.xpos -= 0
-            self.ypos += 1
