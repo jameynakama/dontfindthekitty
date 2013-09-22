85e741eaa3b949af0ec8c988c564dd0ef8456105
Jamey J. DeOrio <jameyd@darkhorse.com>
2013-09-21 22:39:18 -0700

    Changed xpos, ypos, width, height to region
    Region is a tuple of the 4 values (10, 2, 5, 5)

--- src/classes/panel.py
+++ src/classes/panel.py
@@ -1,20 +1,24 @@
+from helpers.constants import Constants
+
+
 class Panel(object):
     """
     Creates a panel that can be drawn to the screen. It can be made with or without a border or shadow.
     """
-    def __init__(self, xpos, ypos, width, height, fgcolor, bgcolor, border=False, shadow=False):
-        self.width = width
-        self.height = height
-        self.xpos = xpos
-        self.ypos = ypos
+    def __init__(self, region, fgcolor, bgcolor, text=None, border=False, shadow=False):
+        self.xpos = region[0]
+        self.ypos = region[1]
+        self.width = region[2]
+        self.height = region[3]
         self.fgcolor = fgcolor
         self.bgcolor = bgcolor
         self.border = border
         self.shadow = shadow
+        self.text = text
 
     def draw(self, window):
         window.fill(
-            ' ',
+            '',
             fgcolor=self.fgcolor,
             bgcolor=self.bgcolor,
             region=(self.xpos, self.ypos, self.width, self.height)
@@ -34,3 +38,6 @@ class Panel(object):
             window.putchar('+', x=self.xpos + self.width - 1, y=self.ypos + self.height - 1, fgcolor='white')
             window.putchar('+', x=self.xpos, y=self.ypos + self.height - 1, fgcolor='white')
             window.putchar('+', x=self.xpos, y=self.ypos, fgcolor='white')
+
+        if self.text:
+            window.write(self.text, x=self.xpos, y=self.ypos, fgcolor=self.fgcolor, bgcolor=self.bgcolor)
