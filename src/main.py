d11f8d3f1a6fe98e6b787ddeadea9b4fc0d59ca7
Jamey J. DeOrio <jameyd@darkhorse.com>
2013-09-23 20:03:27 -0700

    Implement win/lose conditions

--- src/main.py
+++ src/main.py
@@ -23,26 +23,28 @@ from classes.zoo_map import ZooMap
 class Game(object):
     def __init__(self):
         self.window = pygcurse.PygcurseWindow(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT, "Don't Find The Kitty")
+        pygame.mouse.set_visible(False)
+        self.window.autoupdate = False
+        self.clock = pygame.time.Clock()
+
         font_name = Constants.CONFIG.get('game', 'font')
         font_size = Constants.CONFIG.getint('game', 'font_size')
         self.window.font = pygame.font.Font(os.path.join(Constants.RES_DIR, font_name), font_size)
-        self.window.autoupdate = False
-        self.clock = pygame.time.Clock()
-        pygame.mouse.set_visible(False)
 
         self.zookeeper = Zookeeper(Constants.CONFIG.get('zookeeper', 'character'))
-        self.creatures = [Creature() for n in range(5)]
+        self.creatures = [Creature() for n in range(3)]
         self.kitty = Creature(creature='kitty')
+        self.creatures.append(self.kitty)
         self.zoo_map = ZooMap()
-        self.zoo_map.place_creatures(self.creatures + [self.kitty])
+        self.zoo_map.place_creatures(self.creatures)
         self.message_panel = MessagePanel(0, Constants.ZOO_HEIGHT)
         self.last_captured = Creature()
 
-        self.lost = False
+        self.exit_game_loop = False
 
     def run(self):
 
-        while 1:
+        while not self.exit_game_loop:
             self.clock.tick(Constants.FPS)
 
             self.window.setscreencolors(None, 'black', clear=True)
@@ -59,14 +61,18 @@ class Game(object):
                         captured = self.zookeeper.capture(self.zoo_map, xpos, ypos)
                         if captured:
                             self.last_captured = captured
+                            self.creatures.remove(captured)
+                        if self.last_captured.creature == 'kitty':
+                            self.end_screen(won=False)
+                            break
+                        if len(self.creatures) == 1:
+                            self.end_screen(won=True)
+                            break
                     for button in self.message_panel.buttons:
                         if button.contains(xpos, ypos):
                             button.action()
 
             # compute
-            if self.last_captured and self.last_captured.creature == 'kitty':
-                self.end_screen()
-
             for row in self.zoo_map.grid:
                 for thing in row:
                     if thing and random.choice(range(Constants.CONFIG.getint('creatures', 'chance_to_move'))) == 1:
@@ -81,14 +87,16 @@ class Game(object):
         self.zookeeper.draw(self.window, pygame.mouse.get_pos())
         self.window.update()
 
-    def end_screen(self):
+    def end_screen(self, won=False):
         kitty_panel = Panel(region=(12, 5, 36, 5), fgcolor='white', bgcolor='gray', border=True, shadow=True)
-        kitty_panel.text = 'Play again?'
+        kitty_panel.text = "You won!" if won else "You lost!"
+        kitty_panel.text += ' Play again?'
 
         buttons = []
 
         yes_button = Button(region=(18, 8, 6, 1), fgcolor=pygame.Color(0, 100, 0), bgcolor='white')
         yes_button.text = ' yes'
+        yes_button.action = lambda: self.restart()
         buttons.append(yes_button)
 
         no_button = Button(region=(28, 8, 6, 1), fgcolor=pygame.Color(100, 0, 0), bgcolor='white')
@@ -98,7 +106,8 @@ class Game(object):
 
         tweet_button = Button(region=(38, 8, 6, 1), fgcolor=pygame.Color(0, 0, 255), bgcolor='white')
         tweet_button.text = 'tweet'
-        twitter_text = "I found the kitty! {sex} was {adjective}.".format(
+        twitter_text = "I didn't find the kitty!" if won else "I found the kitty!"
+        twitter_text += " {sex} was {adjective}.".format(
             sex=self.kitty.sex.capitalize(),
             adjective=self.kitty.adjective,
         )
@@ -111,7 +120,7 @@ class Game(object):
         tweet_button.action = lambda: webbrowser.open(twitter_url)
         buttons.append(tweet_button)
 
-        while 1:
+        while not self.exit_game_loop:
             self.clock.tick(Constants.FPS)
 
             self.window.setscreencolors(None, 'black', clear=True)
@@ -139,10 +148,14 @@ class Game(object):
             self.zookeeper.draw(self.window, pygame.mouse.get_pos())
             self.window.update()
 
+    def restart(self):
+        self.exit_game_loop = True
+
 
 def main():
-    game = Game()
-    game.run()
+    while 1:
+        game = Game()
+        game.run()
 
 
 if __name__ == '__main__':
